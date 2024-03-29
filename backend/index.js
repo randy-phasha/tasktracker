61f37507ff6e70c7d4778c7a5d79a4145332be3d
Mitch Chanza <mitch.chanza@gmail.com>
2024-03-29 16:00:24 +0200

    🛠️ index.js -> Added import for Datafile 🛠️ index.js -> Added route for /data/:type

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { Datafile } from './data/dataFile.js';
 const app = express();
 app.get('/', (req, res) => { res.send({ message: "Hello, World!", data: [1, 2, 3, 4, 5] }); })
 app.get("/names", (req, res) => { res.send({ message: "Hello, World!", data: ["John", "Jane", "Joe", "Jill"] }); })
-
+app.get("/data/:type", Datafile) // /data/employees, /data/students
 // End of app initialization
 app.listen(6700, () => {
     console.log(`Server listening on port http://localhost:6700`);
