8e4b33ce5cb6c6c5c3b0bdd733f8fced46e60b1a
Mitch Chanza <mitch.chanza@gmail.com>
2024-04-10 17:02:12 +0200

    🛠️ [.gitignore] -> Added .env to ignore list 🛠️ [backend/index.js] -> Enhanced API with dotenv, error handling, and dynamic port configuration

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,37 @@
 import express from 'express';
 import { Datafile } from './data/dataFile.js';
-
+import dotenv from 'dotenv'
+// bodyParse
+dotenv.config()
+const env = process.env
 // 1. REST API
 const app = express();
+const localDb = []
+app.use(express.json())
 // adding a method or endpoint requires the following:
 // endpoint(route) name : string
 // callback method : function  -> two params (reciceing:tre, sending data:res)
 app.get('/', (req, res) => { res.send({ message: "Hello, World!", data: [1, 2, 3, 4, 5] }); })
 app.get("/names", (req, res) => { res.send({ message: "Hello, World!", data: ["John", "Jane", "Joe", "Jill"] }); })
 app.get("/data/:type", Datafile) // /data/employees, /data/students
-app.patch('/data', (a,b)=>{
-    // a= {headers, body}
-    // const headers = a.headers
-
-    const name = "Clynton Phasha Chanza"
-    const [firstName,middleName,lastName] = name.split(" ")
-    // const body = a.body
-    const {headers,body}= a
-    // object|array desctructuring
+app.patch('/data', (a, b) => {
+    const { body } = a
+    console.log(body)
+    return b.send({ fulName: body.firstName + " " + body.lastName, message: "Recived data", success: true })
 })
+app.post("/postdata", (requestdata, responsedata) => {
+    const { body:bodyData } = requestdata
+    try {
+        localDb.push(bodyData)
+        return responsedata.send({ message: "Recived data", success: true, data: localDb })
+    } catch (error) {
+        return responsedata.status(500).send({ message: "Data not Recived", success: false })
+    }
+})
+console.log(env.API_PORT)
 // End of app initialization
-app.listen(6700, () => {
-    console.log(`Server listening on port http://localhost:6700`);
+app.listen(env.API_PORT ?? 6700, () => {
+    console.log(`Server listening on port http://localhost:${env.API_PORT ?? 6700}`);
 })
 // GET, POST, PUT, PATCH, DELETE : HTTP methods
 // use 
