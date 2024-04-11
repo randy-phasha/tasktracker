6ed35b4beaf8a288e284362f3d8acf0f4da9902e
randy-phasha <randy.phasha@mail.com>
2024-04-11 16:10:26 +0200

    feat:created employee.js

---
+++ backend/employee.js
@@ -0,0 +1,42 @@
+export const EmployeeData = [2,4, 6, "Mitch", "Clynton", { name: "Clynton" }, [1, 2]] // length, pop, push, shift, map, filter
+const length = EmployeeData.length // 4
+// get the last item
+const lastInem = EmployeeData[length - 1] // "Clynton"
+EmployeeData.pop() // remove the last item
+//console.log(EmployeeData) // [2, 4, 6, "Mitch"]
+EmployeeData.push("Benon") // add a new item
+EmployeeData.shift() // remove the first item
+
+// let, {var},  const
+// let mutatable
+// const immutable
+
+const mappedData = EmployeeData.map(student => {
+    if (typeof student === "string") {
+        return student
+    }
+})
+const fiteredData = EmployeeData.filter(student => typeof student === "number")
+//console.log(mappedData) // ["Mitch", "Benon"]
+
+export const EmployeeDataObject = {
+    name: "Mitch",
+    age: 67,
+    nationality: "South Africa"
+} // keys, values, entries, 
+
+const keys = Object.keys(EmployeeDataObject) // ["name", "age", "nationality"]
+const values = Object.values(EmployeeDataObject) // ["Mitch", 67, "South Africa"]
+const entries = Object.entries(EmployeeDataObject) // [["name", "Mitch"], ["age", 67], ["nationality", "South Africa"]]
+
+// loops map, foreEach, filter, for
+
+
+//v2
+for(let i in EmployeeData) {
+    //console.log(EmployeeData[i])
+}
+// v3
+for(let t of EmployeeData) {    
+    //console.log(t)
+}
